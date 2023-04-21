fd0cf5d84aabf4cd7b91fa7401ae329d19847641
unknown <leonardohchoi@gmail.com>
2023-04-21 13:58:43 -0300

    work on searchBar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-// Access key for the unsplash API
+import SearchBar from "./components/SearchBar";
 
 function App() {
-  function searchImages() = {
-    
-  }
-  return <div>App</div>;
+  const handleSubmit = (term) => {
+    console.log("do a search with", term);
+  };
+
+  return (
+    <div>
+      <SearchBar onSubmit={handleSubmit} />
+    </div>
+  );
 }
 
 export default App;
