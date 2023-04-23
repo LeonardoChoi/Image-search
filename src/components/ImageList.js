0b06a08718714690919a630454cc375c97bde770
Leo Choi <leonardohchoi@gmail.com>
2023-04-23 17:44:42 -0300

    fix searchBar component that was not working

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,5 +1,5 @@
-function ImageList() {
-  return <div></div>;
+function ImageList({ images }) {
+  return <div>ImageList: {images.length}</div>;
 }
 
 export default ImageList;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import useState from "react";
 
 function SearchBar({ onSubmit }) {
   const [term, setTerm] = useState("");
